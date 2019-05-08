283a9a55e9ec70bc3ae58326a7f0384db221070a
zanaka <exray.524@gmail.com>
2019-05-08 15:20:37 +0900

    Modify action name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "hola, mundo!"
   end
 
   def goodbye
